6808876b146a75ada52d2c3afcfc1e0fa53f60eb
LorenaMGvan <8186769+LorenaMGvan@users.noreply.github.com>
2023-04-17 17:32:21 -0500

    inicio rxjs

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -8,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
+  public countries: Country[] = [];
+
+  constructor(private countriesService: CountriesService) {
+  }
+
+  searchByCapital( termino: string): void {
+    this.countriesService.searchCapital(termino)
+      .subscribe( countries => {
+        this.countries = countries;
+        console.log(countries);
+      })
+    // console.log({ termino });
+  }
 }
---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, Observable, of, tap } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({providedIn: 'root'})
+export class CountriesService {
+
+  private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  constructor(private httpClient: HttpClient) { }
+  
+  searchCapital( terminoCapital: string ): Observable<Country[]> {
+    const url = `${ this.apiUrl }/capital/${terminoCapital}`;
+    return this.httpClient.get<Country[]>( url )
+    .pipe(
+      catchError( error => {
+        console.log(error);
+        return of([])
+      })
+    )
+    // .pipe(
+    //   tap( countries => console.log('tap1', countries)),
+    //   map( coutries => [] ),
+    //   tap( countries => console.log('Tap 2', countries)),
+    // )
+  }
+}
